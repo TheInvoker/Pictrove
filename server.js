55b16f0f0840304e1ac31f45e19686e19f1182dc
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2016-06-11 15:13:35 -0400

    update

--- server.js
+++ server.js
@@ -1,16 +1,52 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var havenondemand = require('havenondemand')
 
 
 
-    
+var HPE_HAVEN_KEY = "60219a59-c74e-4a9a-9f58-9342a2586b81";
+var client = new havenondemand.HODClient(HPE_HAVEN_KEY);
 
-
+// check if on mobile
 function isMobile(req) {
     var ua = req.headers['user-agent'];
     return /mobile/i.test(ua);
 }
+
+
+
+
+var data = {'text' : 'I like cats'};
+var jobID;
+
+
+
+function checkJob(jobID) {
+	client.getJobStatus(jobID, function(err, resp, body) {
+		
+		var status = resp.body.status;
+		console.log(status);
+		
+		if (status == 'finished') {
+			handleResult(resp.body.actions[0].result);
+		} else {
+			setTimeout(function() {
+				checkJob(jobID);
+			}, 1000);
+		}
+	});
+}
+function handleResult(result) {
+	console.log(result);
+}
+client.call('analyzesentiment', data, true, function(err,resp,body) {
+	jobID = resp.body.jobID;
+	console.log(jobID);
+	checkJob(jobID);
+}, data);
+
+
 
 
 
