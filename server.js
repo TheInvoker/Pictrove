3491bf48203c421317a5150b30c1027f8498a01a
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2016-06-11 18:17:08 -0400

    changes

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var havenondemand = require('havenondemand')
-
+var https = require('https');
+var request = require("request");
+var Clarifai = require('clarifai');
 
 
 var HPE_HAVEN_KEY = "60219a59-c74e-4a9a-9f58-9342a2586b81";
 var client = new havenondemand.HODClient(HPE_HAVEN_KEY);
+
+var Cclient = new Clarifai({
+  id: 'vHoj4XdPkGsG1ThB_pvn9QrbldygqdJLGiK2d3Xq',
+  secret: 'Cg6MqLWNyRQJNNcOHgBmrniToJFqBNooNGdkWko3'
+})
+
+
+app.use(express.static(__dirname + '/public'));
 
 // check if on mobile
 function isMobile(req) {
@@ -16,6 +26,49 @@ function isMobile(req) {
 
 
 
+function getCredentials() {
+/* 	var options = { 
+		method: 'POST',
+		url: 'https://api.clarifai.com/v1/token/',
+		headers: { 
+			'content-type': 'application/x-www-form-urlencoded',
+			'postman-token': 'dc08176a-927b-1c0f-2bef-60ccef804fea',
+			'cache-control': 'no-cache' 
+		},
+		form: { 
+			client_id: 'vHoj4XdPkGsG1ThB_pvn9QrbldygqdJLGiK2d3Xq',
+			client_secret: 'Cg6MqLWNyRQJNNcOHgBmrniToJFqBNooNGdkWko3',
+			grant_type: 'client_credentials' 
+		} 
+	};
+
+	request(options, function (error, response, body) {
+		if (error) throw new Error(error);
+
+		var data = JSON.parse(body);
+		var access_token = data.access_token;
+		console.log(access_token);
+	}); */
+	
+	Cclient.getAccessToken(function(err, accessToken) {
+		console.log('got token:', accessToken);
+		
+		fs.readFile(__dirname + '/images/472994020_XS.jpg', function(err, data) {
+		  if (err) throw err;
+			console.log('read image');
+			Cclient.tagFromUrls('image', 'http://kickthescale.com/wp-content/uploads/2015/06/Mordy-The-Soccer-Ball.jpg', function(err, results) {
+				if (err) throw err;
+				
+				console.log(results);
+			});
+			
+			
+		});
+
+	});
+}
+
+getCredentials();
 
 
 function checkJob(client, name, jobID, success) {
@@ -28,7 +81,7 @@ function checkJob(client, name, jobID, success) {
 		console.log('Service:', name, 'JobID:', jobID, 'Status:', status);
 		
 		if (status == 'finished') {
-			success(resp.body.actions[0].result);
+			success(resp.body);
 		} else if (status == 'queued') {
 			setTimeout(function() {
 				checkJob(client, name, jobID, success);
@@ -50,15 +103,21 @@ function haven_request(client, name, data, success) {
 }
 
 
-var data = {'text' : 'I like cats'};
-haven_request(client, 'analyzesentiment', data, function(result) {
-	console.log(result);
+/* var data = {'text' : 'I like cats'};
+haven_request(client, 'analyzesentiment', data, function(body) {
+	console.log(body.actions[0].result);
+}); */
+
+
+
+
+
+/* haven_request(client, 'recognizeimages', {
+	'file' : __dirname + '/images/472994020_XS.jpg'
+}, function(body) {
+	console.log(JSON.stringify(body.actions[0]));
 });
-
-
-
-
-app.use(express.static(__dirname + '/public'));
+ */
 
 
 /*
